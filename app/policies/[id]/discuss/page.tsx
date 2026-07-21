1232f365883bd4a22cbcbb4da4be7467e4a2a798
leopura <avillamor@iboninternational.org>
2026-07-21 18:00:17 +0800

    Add max-width constraint to discussions list page
    - Added max-w-4xl container to discussions page
    - Added responsive padding (px-6 on mobile, px-0 on lg)
    - Content now centered with width constraint

--- app/policies/[id]/discuss/page.tsx
+++ app/policies/[id]/discuss/page.tsx
@@ -100,7 +100,7 @@ export default function PolicyDiscussionsPage({
   };
 
   return (
-    <div className="space-y-8">
+    <div className="mx-auto max-w-4xl px-6 lg:px-0 space-y-8">
       {/* Back button */}
       {policy && (
         <Link
